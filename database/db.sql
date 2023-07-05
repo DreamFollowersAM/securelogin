5b4ea37e4cc50299a63ac18e0447804433e798bf
Angel M <dreamfollowersoficial@gmail.com>
2023-07-05 13:23:51 -0600

    corrections

--- database/db.sql
+++ database/db.sql
@@ -18,7 +18,6 @@ CREATE TABLE links (
   url VARCHAR(255) NOT NULL,
   description TEXT,
   user_id INT(11),
-  created_at timestamp NOT NULL DEFAULT current_timestamp,
   CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
 );
 

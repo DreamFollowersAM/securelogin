6feab983f2c560868225aabbd8ec2422a34bdef2
Angel M <90816138+DreamFollowersAM@users.noreply.github.com>
2023-07-05 13:31:07 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     database: {
-        connectionLimit: 10,
+        connectionLimit: 5,
         host: 'bbfedxvyotl4vp62ledt-mysql.services.clever-cloud.com',
         user: 'udfifzkc52a92rdq',
         password: 'JhuXMSdQLmgF5ofz2put',
